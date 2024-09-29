74447f7babad4d8d4c75d2e886e6f60c249aa157
Alexander Rolf <ar223hh@student.lnu.se>
2024-09-29 15:32:57 +0200

    Create a additional class för managing weights and brainstorm what it should do

---
+++ src/classes/WeightManager.js
@@ -0,0 +1,16 @@
+/**
+ * This class manages a collection of Weight instances.
+ */
+export default class WeightManager {
+  /**
+   * Constructor creates a neww instance of WeightManager.
+   */
+  constructor () {
+    this.weights = []
+  }
+  // ska kunna ta bort och lägga till vikter i denna manager. Viktigt med an avarageweight för t ex vågar osv men även kunna
+  // se vad högsta och lägsta varit. Får se vad som hinns med men satsa på att få i ndetta och reflektera över mer efteråt... 
+  // vad behövs egentligen?
+  // tänk vad som kan vara nyttigt...
+
+}
