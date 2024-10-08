e96a3c28955307dcebb1222559551e3c4a9b5e6a
Alexander Rolf <ar223hh@student.lnu.se>
2024-10-08 19:46:30 +0200

    Fix the weightUnits-object after suggestions of improvement from the code review workshop to make it so that users cannot manipulate it

--- src/weightUnits.js
+++ src/weightUnits.js
@@ -1,11 +1,18 @@
 // Define weight units and also their conversion to g (grams).
-const weightUnits = {
+const weightUnits = Object.freeze({
   g: 1,
   kg: 1000,
   mg: 0.001,
   lb: 453.592,
   stone: 6350.29,
   oz: 28.3495
-}
+})
 
-export default weightUnits
+/**
+ * A function that returns the weightUnits object.
+ *
+ * @returns {object} weightUnits object.
+ */
+export function getWeightUnits () {
+  return weightUnits
+}
