11f422efb7caebdde2630c869f14ea49bb824ecc
Alexander Rolf <ar223hh@student.lnu.se>
2024-10-14 22:09:13 +0200

    Fix weightunits import

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ import Weight from './src/classes/Weight.js'
 import WeightCollection from './src/classes/WeightCollection.js'
 import { unitValidator } from './src/validator.js'
 import { convertUnit } from './src/unitConverter.js'
-import weightUnits from './src/weightUnits.js'
+import { getWeightUnits } from './src/weightUnits.js'
 
-export { Weight, WeightCollection, convertUnit, unitValidator, weightUnits }
+export { Weight, WeightCollection, convertUnit, unitValidator, getWeightUnits }
